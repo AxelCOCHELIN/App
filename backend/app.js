b97a6770b8eee482171ba9f912e37ef5796f32d4
Axel COCHELIN <ax.cochelin@gmail.com>
2022-01-20 22:08:07 +0100

    test serveur et  stabilisation serveur

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,23 @@ const express = require("express");
 const app = express();
 
 //création d'une route initiale
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("requête reçue!");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: "Votre requête à bien été reçu." });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("La réponse a été envoyée avec succès");
 });
 
 //export de l'application pour y accéder depuis les autres fichiers
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,55 @@ const http = require("http");
 // import de l'application
 const app = require("./app");
 
+// fonction renvoyant un port valide
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
 //initialisation du port
-app.set("port", process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || 3000);
+app.set("port", port);
+
+// fonction recherchant les différentes erreurs et qui les gère de manière appropriée.
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + "requires elevetad privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
 // création du serveur
 const server = http.createServer(app);
+
+// configuration d'un écouteur d'évènements consignant le port ou le canal sur lequel le serveur s'exécute
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port: " + port;
+  console.log("Listening on " + bind);
+});
 
 // écoute du serveur
 
