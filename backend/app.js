afb84d6b3ea65162108e7fa10b8e62cc0d90e648
Axel COCHELIN <ax.cochelin@gmail.com>
2022-01-20 22:21:22 +0100

    création d'une API simple

--- backend/app.js
+++ backend/app.js
@@ -6,24 +6,27 @@ const express = require("express");
 //création de l'application express
 const app = express();
 
-//création d'une route initiale
-app.use((req, res, next) => {
-  console.log("requête reçue!");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête à bien été reçu." });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("La réponse a été envoyée avec succès");
+//création d'une API
+app.use("/api/object", (req, res, next) => {
+  const object = [
+    {
+      _id: "Obj1",
+      title: "Le premier objet",
+      description: "Les infos du premier objet",
+      imageUrl: "",
+      price: 4900,
+      userId: "userOne",
+    },
+    {
+      _id: "Obj2",
+      title: "Le second objet",
+      description: "Les infos du second objet",
+      imageUrl: "",
+      price: 2900,
+      userId: "userOne",
+    },
+  ];
+  res.status(200).json(object);
 });
 
 //export de l'application pour y accéder depuis les autres fichiers
